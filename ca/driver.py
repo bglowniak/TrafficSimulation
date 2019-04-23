316c72085c5dde75afd6effbb33699b907c02383
jkatz35 <k.joel969@gmail.com>
2019-04-22 22:09:22 -0400

    added some statistics and added scale factor

--- ca/driver.py
+++ ca/driver.py
@@ -15,7 +15,3 @@ lights = {20: s10, 40: s11, 80: s12, 110: s13, 140: s14}
 
 road1 = Two_Lane(150, lights)
 road1.simulate(1000, 5)
-
-#lane1 = Lane(151)
-#lane1.add_stoplights(lights)
-#lane1.simulate(500,2)
--- ca/stats.py
+++ ca/stats.py
@@ -7,6 +7,8 @@ class Stats():
     def __init__(self):
         self.seed = 0
         self.vehicle_exit_times = []
+        self.vehicle_starts = []
+        self.vehicle_ends = []
 
     def pick_lane(self):
         if random.random() < .5:
@@ -14,8 +16,8 @@ class Stats():
         else:
             return 1
 
-    def generate_vehicle(self, sim_time):
-        inter_arrival, velocity, entrance, exit_point = spawn_vehicle()
+    def generate_vehicle(self, sim_time, sf=1):
+        inter_arrival, velocity, entrance, exit_point = spawn_vehicle(sf=sf)
         velocity = velocity//5
         if velocity > 5:
             velocity = 5
@@ -25,6 +27,10 @@ class Stats():
         if sim_time < 100:
             return
         self.vehicle_exit_times.append(sim_time - vehicle.get_enter_time())
+        self.vehicle_starts.append(vehicle.get_source())
+        self.vehicle_ends.append(vehicle.get_dest())
 
     def calculate_stats(self):
-        return sum(self.vehicle_exit_times)/ len(self.vehicle_exit_times)
+        avg = sum(self.vehicle_exit_times)/ len(self.vehicle_exit_times)
+        print('average: {:.2f}'.format(avg))
+        return
